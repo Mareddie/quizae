9ac28211a244798cdd2a7d816254afaea4e446d0
Martin Mička <hey@martinmicka.me>
2022-11-06 10:43:31 +0100

    Implemented question category e2e tests

--- test/question-category.e2e-spec.ts
+++ test/question-category.e2e-spec.ts
@@ -36,7 +36,118 @@ describe('Question Categories', () => {
     await request(app.getHttpServer())
       .get(`/question-categories/${testData.group.id}`)
       .expect(401);
+
+    await request(app.getHttpServer())
+      .post(`/question-categories/${testData.group.id}/create`)
+      .expect(401);
+
+    await request(app.getHttpServer())
+      .patch(
+        `/question-categories/${testData.group.id}/${testData.categories[0].id}`,
+      )
+      .expect(401);
+
+    await request(app.getHttpServer())
+      .delete(
+        `/question-categories/${testData.group.id}/${testData.categories[0].id}`,
+      )
+      .expect(401);
   });
 
-  // TODO: implement tests
+  it('lists question categories', async () => {
+    const listResponse = await request(app.getHttpServer())
+      .get(`/question-categories/${testData.group.id}`)
+      .set('Authorization', `Bearer ${authToken}`)
+      .set('Accept', 'application/json');
+
+    expect(listResponse.statusCode).toEqual(200);
+    expect(listResponse.headers['content-type']).toMatch(/json/);
+
+    expect(listResponse.body).toBeInstanceOf(Array);
+    expect(listResponse.body[0]).toMatchObject(testData.categories[0]);
+    expect(listResponse.body[1]).toMatchObject(testData.categories[1]);
+  });
+
+  it('creates question category', async () => {
+    const createResponse = await request(app.getHttpServer())
+      .post(`/question-categories/${testData.group.id}/create`)
+      .set('Authorization', `Bearer ${authToken}`)
+      .set('Accept', 'application/json')
+      .send({ name: 'Created Question Category', order: 3 });
+
+    expect(createResponse.statusCode).toEqual(201);
+    expect(createResponse.headers['content-type']).toMatch(/json/);
+
+    expect(createResponse.body).toMatchObject({
+      id: expect.any(String),
+      name: 'Created Question Category',
+      groupId: testData.group.id,
+      order: 3,
+    });
+
+    const createdQuestionCategory = await prisma.questionCategory.findUnique({
+      where: {
+        id: createResponse.body.id,
+      },
+    });
+
+    expect(createdQuestionCategory).toMatchObject({
+      id: createResponse.body.id,
+      name: createResponse.body.name,
+      groupId: createResponse.body.groupId,
+      order: createResponse.body.order,
+    });
+  });
+
+  it('updates question category', async () => {
+    const updateResponse = await request(app.getHttpServer())
+      .patch(
+        `/question-categories/${testData.group.id}/${testData.categories[0].id}`,
+      )
+      .set('Authorization', `Bearer ${authToken}`)
+      .set('Accept', 'application/json')
+      .send({ name: 'Updated Question Category', order: 55 });
+
+    expect(updateResponse.statusCode).toEqual(200);
+    expect(updateResponse.headers['content-type']).toMatch(/json/);
+
+    expect(updateResponse.body).toMatchObject({
+      id: testData.categories[0].id,
+      name: 'Updated Question Category',
+      groupId: testData.group.id,
+      order: 55,
+    });
+
+    const updatedQuestionCategory = await prisma.questionCategory.findUnique({
+      where: {
+        id: updateResponse.body.id,
+      },
+    });
+
+    expect(updatedQuestionCategory).toMatchObject({
+      id: updateResponse.body.id,
+      name: updateResponse.body.name,
+      groupId: updateResponse.body.groupId,
+      order: updateResponse.body.order,
+    });
+  });
+
+  it('deletes question category', async () => {
+    const deleteResponse = await request(app.getHttpServer())
+      .delete(
+        `/question-categories/${testData.group.id}/${testData.categories[0].id}`,
+      )
+      .set('Authorization', `Bearer ${authToken}`)
+      .set('Accept', 'application/json');
+
+    expect(deleteResponse.statusCode).toEqual(204);
+
+    const deletedQuestionCategory = await prisma.questionCategory.findUnique({
+      where: {
+        id: testData.categories[0].id,
+      },
+    });
+
+    expect(deletedQuestionCategory).toBeNull();
+  });
 });
