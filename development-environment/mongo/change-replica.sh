a40bccdb656d0fa58e83e6fcc6ec675bf10c83ab
Martin Mička <hey@martinmicka.me>
2023-04-05 11:34:35 +0200

    Changed replica script

--- development-environment/mongo/change-replica.sh
+++ development-environment/mongo/change-replica.sh
@@ -1,4 +1,4 @@
-myhost="mongodb:27017"
+myhost="quizae-mongodb:27017"
 rsConfig=$(mongosh --username "root" --password "root" --authenticationDatabase "admin" --eval "rs.config()" --quiet)
 
 mongosh --username "root" --password "root" --authenticationDatabase "admin" --quiet <<EOF
@@ -6,3 +6,6 @@ const conf = $rsConfig
 conf.members[0].host="$myhost"
 rs.reconfig(conf,{force:true})
 EOF
+
+# For manual host update via mongosh:
+# const config = rs.config(); config.members[0].host = "quizae-mongodb"; rs.reconfig(config, {force: true});
