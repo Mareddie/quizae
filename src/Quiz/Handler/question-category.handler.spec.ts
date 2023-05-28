676a48514968b14a96aa0edd2f79ecea255b321b
Martin Mička <hey@martinmicka.me>
2023-05-28 20:17:34 +0200

    fix: fixed question category unique constraint exception on update

--- src/Quiz/Handler/question-category.handler.spec.ts
+++ src/Quiz/Handler/question-category.handler.spec.ts
@@ -4,16 +4,13 @@ import { plainToClass } from 'class-transformer';
 import { CreateUpdateQuestionCategoryDTO } from '../DTO/create-update-question-category.dto';
 import { ConflictException } from '@nestjs/common';
 
-describe('CreateUpdateQuestionCategoryHandler', () => {
+describe('QuestionCategoryHandler', () => {
   let handler: QuestionCategoryHandler;
 
   const repositoryMock = {
     createForUser: jest.fn().mockResolvedValue({ test: true }),
     updateQuestionCategory: jest.fn().mockResolvedValue({ test: true }),
-    fetchForUser: jest
-      .fn()
-      .mockResolvedValueOnce([{ id: '123', name: 'test category' }])
-      .mockResolvedValue([]),
+    fetchForUser: jest.fn().mockResolvedValue([]),
     fetchById: jest.fn().mockResolvedValue({
       id: '123',
       userId: '123',
@@ -48,6 +45,10 @@ describe('CreateUpdateQuestionCategoryHandler', () => {
 
   describe('createQuestionCategory', () => {
     it('throws exception on existing similar question category', async () => {
+      repositoryMock['fetchForUser'].mockResolvedValueOnce([
+        { id: '123', name: 'test category' },
+      ]);
+
       const dto = plainToClass(CreateUpdateQuestionCategoryDTO, {
         name: 'Test Category',
         order: 1,
@@ -89,12 +90,16 @@ describe('CreateUpdateQuestionCategoryHandler', () => {
 
   describe('updateQuestionCategory', () => {
     it('updates question category', async () => {
+      repositoryMock['fetchForUser'].mockResolvedValueOnce([
+        { id: '123', name: 'test category' },
+      ]);
+
       const dto = plainToClass(CreateUpdateQuestionCategoryDTO, {
         name: 'This Is Great Update',
         order: 1,
       });
 
-      const result = await handler.updateQuestionCategory('123', '111', dto);
+      await handler.updateQuestionCategory('123', '111', dto);
 
       expect(repositoryMock['fetchForUser']).toHaveBeenCalledTimes(1);
       expect(repositoryMock['fetchForUser']).toHaveBeenCalledWith(
@@ -106,6 +111,31 @@ describe('CreateUpdateQuestionCategoryHandler', () => {
       expect(repositoryMock['updateQuestionCategory']).toHaveBeenCalledWith(
         '123',
         dto,
+      );
+    });
+
+    it('resolves potentially unique constraint exception', async () => {
+      repositoryMock['fetchForUser'].mockResolvedValueOnce([
+        {
+          id: '1234',
+          name: 'This Is Great Update',
+          order: null,
+        },
+      ]);
+
+      const dto = plainToClass(CreateUpdateQuestionCategoryDTO, {
+        name: 'This Is Great Update',
+        order: 1,
+      });
+
+      await expect(
+        handler.updateQuestionCategory('123', '111', dto),
+      ).rejects.toThrow(ConflictException);
+
+      expect(repositoryMock['fetchForUser']).toHaveBeenCalledTimes(1);
+      expect(repositoryMock['fetchForUser']).toHaveBeenCalledWith(
+        '111',
+        'This Is Great Update',
       );
     });
   });
